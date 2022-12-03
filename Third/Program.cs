9b1798581098f609a8a50c1c760519b480c66c06
Gracjan Grochowski <gracjan.grochowski@mesh-systems.com>
2022-12-03 12:17:05 +0100

    third

---
+++ Third/Program.cs
@@ -0,0 +1,24 @@
+﻿var sumOfFirstTask = 0;
+var groupRucksack = File.ReadLines("./data.txt").ToList();
+foreach (var line in groupRucksack)
+{
+    var firstRucksack = line[..(line.Length / 2)];
+    var secondRucksack = line.Substring(line.Length / 2, line.Length / 2);
+
+    var commonCharacters = firstRucksack.Intersect(secondRucksack).SingleOrDefault();
+    sumOfFirstTask += CheckPriorities(commonCharacters);
+}
+
+var secondTaskSum = groupRucksack
+    .Chunk(3)
+    .Select(group => group[0].Intersect(group[1]).Intersect(group[2])
+        .ToList())
+    .Select(common => CheckPriorities(common.Single())).Sum();
+
+Console.WriteLine($"FirstTaskSum: {sumOfFirstTask}");
+Console.WriteLine($"SecondTaskSum: {secondTaskSum}");
+
+int CheckPriorities(char c)
+{
+    return char.IsLower(c) ? c < 97 ? c - 64 : c - 96 : (c - 64) + 26;
+}
